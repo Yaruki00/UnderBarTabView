e8fe8a4a8d0aea9e3a734616fc118b9965926cb1
Yuta Kawabe <yuyu3165@gmail.com>
2019-10-30 18:06:31 +0900

    fix sample

--- UnderBarTab/Presentation/SampleViewController.swift
+++ UnderBarTab/Presentation/SampleViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SampleViewController.swift
 //  UnderBarTab
 //
 //  Created by Yuta Kawabe on 2019/10/30.
@@ -9,12 +9,20 @@
 import UIKit
 import RxSwift
 
-class ViewController: UIViewController {
+enum SampleType {
+    case normal
+    case infinite
+}
 
-    @IBOutlet private weak var tabView: UnderBarInfiniteTabView!
+final class SampleViewController: UIViewController {
+
+    @IBOutlet private weak var tabView: UnderBarTabView!
+    @IBOutlet private weak var infiniteTabView: UnderBarInfiniteTabView!
     @IBOutlet private weak var scrollView: UIScrollView!
     
-    let disposeBag = DisposeBag()
+    var type: SampleType = .normal
+    
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,20 +31,31 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController {
+extension SampleViewController {
     
     private func setupTabView() {
+        switch self.type {
+        case .normal:
+            self.tabView.isHidden = false
+            self.infiniteTabView.isHidden = true
+        case .infinite:
+            self.tabView.isHidden = true
+            self.infiniteTabView.isHidden = false
+        }
         self.tabView.setup(type: .fixed(width: UIScreen.main.bounds.width / 5))
+        self.infiniteTabView.setup(type: .fixed(width: UIScreen.main.bounds.width / 5))
         self.tabView.setData(["1", "2", "3", "4", "5"])
+        self.infiniteTabView.setData(["1", "2", "3", "4", "5"])
     }
     
     private func bind() {
         let pageChanged = Observable.merge(
             tabView.rx.tapAt.asObservable(),
+            infiniteTabView.rx.tapAt.asObservable(),
             scrollView.rx.didEndDecelerating.map { Int(self.scrollView.contentOffset.x / self.scrollView.frame.width) }
         ).distinctUntilChanged().share()
             
-        pageChanged.bind(to: tabView.rx.selectedIndex).disposed(by: disposeBag)
+        pageChanged.bind(to: tabView.rx.selectedIndex, infiniteTabView.rx.selectedIndex).disposed(by: disposeBag)
             
         pageChanged.map { (CGPoint(x: CGFloat($0) * self.scrollView.frame.width, y: 0.0), true) }
             .bind(onNext: scrollView.setContentOffset)
--- UnderBarTab/Presentation/SelectTypeViewController.swift
+++ UnderBarTab/Presentation/SelectTypeViewController.swift
@@ -7,7 +7,29 @@
 //
 
 import Foundation
+import UIKit
 
-final class SelectTypeViewController {
+final class SelectTypeViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func transitToNormal(_ sender: Any) {
+        self.transitToSampleView(title: "normal", type: .normal)
+    }
+    
+    @IBAction func transitToInfinite(_ sender: Any) {
+        self.transitToSampleView(title: "infinite", type: .infinite)
+    }
+}
+
+extension SelectTypeViewController {
+    
+    private func transitToSampleView(title: String, type: SampleType) {
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "Sample") as! SampleViewController
+        vc.title = title
+        vc.type = type
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
